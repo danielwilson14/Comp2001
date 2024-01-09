d2bcc9fb5f30f44e69298797e17fbb6a655d8e1f
danielwilson14 <114652856+danielwilson14@users.noreply.github.com>
2024-01-09 00:06:49 +0000

    anyone can see anyone with limited view, users only edited by creator, editing limited correctly, worked through RESTful API principles, added links to DTOs, added possible next actions to the return statement of every GET call, must be an admin to delete a location

--- Program.cs
+++ Program.cs
@@ -13,6 +13,11 @@ builder.Services.AddSingleton<TokenService>();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "Your API", Version = "v1" });
+});
+
 
 // Configure JWT Authentication
 var key = Encoding.ASCII.GetBytes(builder.Configuration["JwtConfig:Secret"]); 
@@ -36,6 +41,14 @@ builder.Services.AddAuthentication(x =>
 
 // Building and running the application.
 var app = builder.Build();
+
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Your API V1");
+    c.RoutePrefix = "swagger";
+});
 
 app.UseHttpsRedirection();
 
